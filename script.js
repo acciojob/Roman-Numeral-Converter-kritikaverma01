65b82068b9efba8d3a4f170c09cc76ef05170847
kritikaverma01 <Kritikav807@gmail.com>
2026-09-13 14:20:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
+javascript
 function convertToRoman(num) {
-  const obj = {
-    0: ['M', 1000],
-    1: ['D', 500],
-    2: ['C', 100],
-    3: ['L', 50],
-    4: ['X', 10],
-    5: ['V', 5],
-    6: ['I', 1]
-  };
+  const symbols = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
   let result = "";
 
-  // Go through the symbols from largest to smallest
-  for (let i = 0; i < 7; i++) {
-    let symbol = obj[i][0];
-    let value = obj[i][1];
+  for (let i = 0; i < symbols.length; i++) {
+    const symbol = symbols[i][0];
+    const value = symbols[i][1];
 
-    let count = Math.floor(num / value);
+    const count = Math.floor(num / value);
 
     result += symbol.repeat(count);
-
     num = num % value;
   }
 
